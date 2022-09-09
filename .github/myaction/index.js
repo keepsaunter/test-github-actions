c9539a9fbcb34d9a9b06be0f5efdeb4695df1bca
keepsaunter <2336990515@qq.com>
2022-09-09 16:35:22 +0800

    test

--- .github/myaction/index.js
+++ .github/myaction/index.js
@@ -1,4 +1,4 @@
 // const core = require('@actions/core');
 // const github = require('@actions/github');
 // console.log(90090, core.getInput(testName))
-console.log(90090, process.env.INPUT_testName)
+console.log(90090, process.env)
