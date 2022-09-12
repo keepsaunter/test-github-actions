b4fa99131cbff7ab16f491611ec373f4cd5199ef
keepsaunter <2336990515@qq.com>
2022-09-12 16:16:51 +0800

    create

--- .github/myaction/src/index.js
+++ .github/myaction/src/index.js
@@ -2,6 +2,5 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 core.setOutput('test99', 'this is test output');
-// console.log(JSON.stringify(github.context.payload, undefined, 2));
-console.log(github.context.payload);
+console.log(JSON.stringify(github, undefined, 2));
 console.log(90090, core.getInput('testName') + ' this is test2');
