14ee95aeb1e194926fe829008573e310f7bd40de
keepsaunter <2336990515@qq.com>
2022-09-14 15:59:45 +0800

    test

--- .github/myaction/src/index.js
+++ .github/myaction/src/index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-core.setOutput('test99', 'this is test output');
+// core.setOutput('test99', 'this is test output');
 console.log(JSON.stringify(github, undefined, 2));
 console.log(90090, core.getInput('testName') + ' this is test2');
